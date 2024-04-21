d7a0fdb32dd98989b835b530a10843f96c25e225
nwesseli <nwesseli@iu.edu>
2024-04-21 11:58:48 -0400

    m

--- configuration.js
+++ configuration.js
@@ -53,12 +53,14 @@ async function updateTheNavigationBar() {
 async function signup() {
     let email = document.getElementById("email").value;
     let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    let customer = {email:email, username: username, password: password}
+    let password = document.getElementById("passs").value;
+    let customer = {email:email, username: username, password: password};
+    console.log(customer);
+    
     let request = {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         },
         body: JSON.stringify(customer)
       };
@@ -82,34 +84,35 @@ async function signup() {
 
 
 async function login() {    
-    let username = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    let customer = {username: username, password: password}
+    let username = document.getElementById("user").value;
+    let password = document.getElementById("pass").value;
+    let customer = {username: username, password: password};
+    
     let request = {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify(customer)
-      };
-      try {
+    };
+    try{
         let response = await fetch(getHost() + "/signin", request);
-        if(response.status == 200) {  
-            alert("The login was successful!");
+        if(response.status == 200){
+            alert("The login was succesful!");
             const token = await response.text();
-            saveTheToken(token);            
+            saveTheToken(token);
             location.href = "index.html";
-        } else {
-            console.log(`response status:${response.status}`);   
-            removeTheToken();         
-            alert("Something went wrong!");
-        }
-      }
-      catch(error) {
-        console.log(error); 
-        removeTheToken();       
-        alert("Something went wrong!");
-      }    
+        }else{
+            console.log(`response status:${response.status}`);
+            removeTheToken();
+            alert("Something went wrong")
+        }   
+    }
+    catch(error){
+        console.log(error);
+        removeTheToken();
+        alert("Something went wrong")
+    }
 }
 
 async function logout() {   
